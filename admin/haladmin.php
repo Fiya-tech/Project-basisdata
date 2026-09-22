cec15702393130f34399776882c481821f5a5967
Fiya-nk <4lfiy4nk@gmail.com>
2026-09-22 11:25:10 +0700

    Add files via upload
    Penambahan fitur pada halaman admin

---
+++ admin/haladmin.php
@@ -0,0 +1,14 @@
+<?php
+include('cekadmin.php');
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Admin</title>
+        <title>Admin DashBoard</title>
+    </head>
+<body>
+        <h2>Ini Halaman Khusus Admin</h2>
+        <a href="logout.php">Logout</a>
+</body>
+</html>
